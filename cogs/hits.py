15aa1b170333585c778e1db0963316a2079ef038
siquejontop <angelpro1234la@gmail.com>
2025-09-27 10:43:26 -0500

    Update hits.py

--- cogs/hits.py
+++ cogs/hits.py
@@ -181,7 +181,7 @@ class Hits(commands.Cog):
     # Español
     @commands.command()
     async def hits(self, ctx):
-        REQUIRED_ROLE_IDS = [1415860204624416971, 1412083394614923315]  # 👈 roles mínimos permitidos
+        REQUIRED_ROLE_IDS = [1421330888192561152]  # 👈 roles mínimos permitidos
         required_roles = [ctx.guild.get_role(rid) for rid in REQUIRED_ROLE_IDS if ctx.guild.get_role(rid)]
 
         # ✅ Verifica si el usuario tiene un rol igual o superior a cualquiera de los requeridos
